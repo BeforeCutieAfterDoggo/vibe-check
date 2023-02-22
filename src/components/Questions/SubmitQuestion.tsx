ce23a1027da2eadf21e19c8cba1cca052731f223
Lucy Qiu <lucy1049684100@gmail.com>
2023-02-22 00:20:27 -0800

    s

--- src/components/Questions/SubmitQuestion.tsx
+++ src/components/Questions/SubmitQuestion.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import axios from "axios";
 import { handleAxiosError } from "../../lib/fetcher";
 import { QuestionType } from "../../types";
@@ -6,7 +6,9 @@ import { Select } from "antd";
 import MultipleChoiceQuestionForm from "./MultipleChoiceQuestionForm";
 import ScaleQuestionForm from "./ScaleQuestionForm";
 import ShortAnswerQuestionForm from "./ShortAnswerQuestionForm";
-
+import Link from "next/link";
+import { useContext, useState } from "react";
+import { useRouter } from 'next/router';
 const questionTypeLabelMap = {
   [QuestionType.SCALE]: "Scale",
   [QuestionType.MULTIPLE_CHOICE]: "Multiple Choice",
@@ -16,19 +18,30 @@ const questionTypeLabelMap = {
 const questionTypeComponentMap = {
   [QuestionType.SCALE]: <ScaleQuestionForm />,
   [QuestionType.MULTIPLE_CHOICE]: <MultipleChoiceQuestionForm />,
-  [QuestionType.SHORT_ANSWER]: <ShortAnswerQuestionForm/>
+  [QuestionType.SHORT_ANSWER]: <ShortAnswerQuestionForm />
 };
 
 const SubmitQuestion = () => {
   const [questionType, setQuestionType] = useState<QuestionType>(
     QuestionType.SHORT_ANSWER
   );
+  const router = useRouter();
+  const link = router.asPath;
+  console.log("dynamicParam", link)
+
 
   return (
     <>
       <h1 className="text-center text-white italic underline decoration-wavy decoration-4 tracking-wide font-bold  text-3xl font-serif py-10 ">
         Submit a Question</h1>
-
+      <div className="flex justify-center mb-10">
+        <Link href={`/${link}/dashboard`}>
+          <button className="bg-black text-white font-bold py-2 px-4 font-serif rounded-full mr-4">View VibeBoard </button>
+        </Link>
+        <Link href={`/`}>
+          <button className="bg-black text-white font-bold py-2 px-4 font-serif rounded-full">Home</button>
+        </Link>
+      </div>
       <div className="max-w-md mx-auto p-4 bg-white rounded-md shadow-md">
         <Select value={questionType} onChange={setQuestionType}>
           {Object.values(QuestionType).map((type, idx) => (
--- src/pages/sessions/[session]/dashboard.tsx
+++ src/pages/sessions/[session]/dashboard.tsx
@@ -1,5 +1,5 @@
 // pages/dashboard.tsx
-
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import VisualCards from "../../../components/Dashboard/VisualCards";
@@ -8,7 +8,10 @@ import {
 } from "../../../providers/SessionProvider";
 const Dashboard: React.FC = () => {
   const router = useRouter();
-  
+  const link = router.asPath;
+  const result = link.slice(0, link.lastIndexOf('/'));
+
+  console.log("vibe", result)
 
   return (
     <SessionProvider sessionId={router.query.session as string}>
@@ -18,6 +21,14 @@ const Dashboard: React.FC = () => {
             <h1 className="text-white text-5xl font-bold mb-8 underline decoration-wavy decoration-4 tracking-wide italic font-serif text-center">
               Vibeboard
             </h1>
+            <div className="flex justify-center mb-10">
+              <Link href={`/${result}`}>
+                <button className="bg-black text-white font-bold py-2 px-4 font-serif rounded-full mr-4">Back to Session</button>
+              </Link>
+              <Link href={`/`}>
+                <button className="bg-black text-white font-bold py-2 px-4 font-serif rounded-full">Home</button>
+              </Link>
+            </div>
           </div>
           <main className=" mx-auto  sm:px-6 lg:px-8 ">
             <VisualCards />
